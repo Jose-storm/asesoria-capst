a02790e313983a26647269bb8937e1ae74c9c099
Jose-storm <josechacondoritorreswork@gmail.com>
2025-07-15 21:26:55 -0500

    Render compatibility aplication

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api", // ajusta según tu backend
+  // baseURL: "http://localhost:4000/api",
+  baseURL: import.meta.env.VITE_API_URL ?? 'http://localhost:4000/api'
 });
 
 API.interceptors.request.use((config) => {
