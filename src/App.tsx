e2f535775d38eeadb06ab4dde44573506ac50275
Jose-storm <josechacondoritorreswork@gmail.com>
2025-07-22 19:45:30 -0500

    Modified today adition pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
 // import {NavigationMenuDemo} from '@/components/Header/Header'
 // App.tsx
+// import AppRouter from "./routes/AppRouter";
+// import "./App.css"
+
+// const EN_MANTENIMIENTO  = import.meta.env.VITE_MANTENIMIENTO === "true"
+// const App = () => <AppRouter />;
+
+// export default App;
 import AppRouter from "./routes/AppRouter";
-import "./App.css"
-const App = () => <AppRouter />;
+import "./App.css";
+import Mantenimiento from "@/pages/support/Mantenimiento"; // Asegúrate de tener esta página
+
+const EN_MANTENIMIENTO = import.meta.env.VITE_MANTENIMIENTO === "true";
+
+const App = () => {
+    if (EN_MANTENIMIENTO) {
+        return <Mantenimiento />;
+    }
+
+    return <AppRouter />;
+};
 
 export default App;
 
